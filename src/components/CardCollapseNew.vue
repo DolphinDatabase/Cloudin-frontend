403e66aed2cad61f12537a981371775a6b1fd9cb
Pedro Motta <pedromotta321r@gmail.com>
2023-03-20 21:51:09 -0300

    #CLD-46 add card component

---
+++ src/components/CardCollapseNew.vue
@@ -0,0 +1,26 @@
+<template>
+    <Disclosure v-slot="{open}">
+        <DisclosureButton class="flex w-full justify-between rounded-lg px-4 py-2 text-gray-200 text-left text-sm font-medium">
+          <span>Preencha os campos cuidadosamente</span>
+          <ChevronUpIcon :class="open ? 'rotate-180 transform' : ''" class="h-5 w-5 text-black"/>
+        </DisclosureButton>
+        <DisclosurePanel class="px-24 pt-4 pb-2 text-sm bg-gray-100">
+          <FormDrive/>
+        </DisclosurePanel>
+    </Disclosure>
+</template>
+<script>
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import FormDrive from '../components/FormDrive.vue'
+import { ChevronUpIcon } from '@heroicons/vue/20/solid'
+export default {
+    name:"CardCollapseNew",
+    components:{
+        Disclosure, 
+        DisclosureButton, 
+        DisclosurePanel,
+        ChevronUpIcon,
+        FormDrive
+    }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
       colors: {
         green: {
           500: '#96DA84'
-        }
+        },
+        gray:{100:'#F2F2F2',200:'RGBA(0,0,0,0.4)'},
       },
       fontFamily: {
         'nunito': ['Nunito', 'sans-serif']
-      }
     },
   },
   plugins: [],
-}
+}}
