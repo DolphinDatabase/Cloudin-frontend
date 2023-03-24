795ab6034142152e93dce122c7158c34007c4738
Borgarelli <borgarellitavares@outlook.com>
2023-03-24 19:44:40 -0300

    bug fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import TransferView from '@/views/TransferView'
-import TransactionView from '../views/TransactionView.vue'
 import SettingsView from '@/views/SettingsView.vue'
 
 const routes = [
@@ -13,11 +12,6 @@ const routes = [
     path: '/transferencia',
     name: 'transferencia',
     component: TransferView
-  },
-  {
-    path: '/transaction',
-    name: 'transaction',
-    component: TransactionView
   }
 ]
 
