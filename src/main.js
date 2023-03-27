5eee0a56e81a311a58e509bb7ede5ec86bd7550a
Renata Garcia Nonato <80370151+RenataGarciaNonato@users.noreply.github.com>
2023-03-27 20:56:32 -0300

    Update main.js
    id_local storage

--- src/main.js
+++ src/main.js
@@ -9,9 +9,20 @@ import './assets/tailwind.css'
 
 const app = createApp(App)
 app.use(vue3GoogleLogin, {
-    clientId: clientSecret.WLc.client_id,
-    scope: 'https://www.googleapis.com/auth/drive'
+  clientId: clientSecret.WLc.client_id,
+  scope: 'https://www.googleapis.com/auth/drive'
 })
+
 app.use(store)
 app.use(router)
+
+// Verifica se a chave "id" existe no local storage
+let id = localStorage.getItem("id")
+
+// Se não existir, gera um número aleatório de 6 posições e salva na chave "id" do local storage
+if (!id) {
+  id = Math.floor(Math.random() * 1000000).toString().padStart(6, '0')
+  localStorage.setItem("id", id)
+}
+
 app.mount('#app')
