92efb682296d486b71fe993c6d6e9f3f8fcd9f43
Hamilton Zanini <hrszanini@gmail.com>
2023-03-28 22:47:12 -0300

    Alterado processo de OAuth2 do Google

--- src/components/auth/GoogleLogin.vue
+++ src/components/auth/GoogleLogin.vue
@@ -1,83 +1,108 @@
 <template>
     <div class="">
-
-        <button type="button" @click="login()"><card-component>
-                <img src="@/assets/auth/Google.svg" class="w-7 mr-2">
-                <p class="cursor-pointer">Autentique com o Google Drive</p>
-            </card-component>
+      <card-component>
+        <button
+          type="button"
+          @click="login()"
+        >
+          <img
+            src="@/assets/auth/Google.svg"
+            class="w-7 mr-2"
+          >
+          <p>Google Drive</p>
         </button>
-
+      </card-component>
     </div>
-</template>
-
-<script>
-import { googleSdkLoaded } from 'vue3-google-login';
-import axios from 'axios';
-import CardComponent from '@/components/CardComponent.vue'
-
-export default {
-    name: 'GoogleLogin',
-    components: {
-        CardComponent
-    },
-    mounted() {
-        if (window.localStorage.getItem("auth")) {
-            this.auth = true
-            this.listFolders()
-        }
-        setInterval(this.refreshToken, 3599000) //a cada 3599s
-    },
-    methods: {
-        login() {
-            {
-                googleSdkLoaded((google) => {
-                    google.accounts.oauth2.initCodeClient({
-                        client_id: '532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com',
-                        scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.metadata https://www.googleapis.com/auth/drive.photos.readonly',
-                        callback: (response) => {
-                            console.log(response)
-                            var data = {
-                                code: response.code,
-                                client_id: "532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
-                                client_secret: "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",
-                                redirect_uri: "http://localhost:8080",
-                                grant_type: "authorization_code"
-                            }
-                            axios.post("https://oauth2.googleapis.com/token", data)
-                                .then(res => {
-                                    window.localStorage.setItem("auth", JSON.stringify(res.data))
-                                    this.refreshToken()
-                                })
-
-                        }
-                    }).requestCode()
-                })
-            }
-        },
-        async refreshToken() {
-            var data = {
-                refresh_token: JSON.parse(window.localStorage.getItem("auth")).refresh_token,
-                client_id: "532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
-                client_secret: "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",
-                grant_type: "refresh_token"
-            }
-            const res = await axios.post("https://oauth2.googleapis.com/token", data)
-
-            console.log(res.data)
-            console.log(data.refresh_token)
-            localStorage.setItem("google", data.refresh_token)
-
-        },
-        async listFolders() {
-            const auth = JSON.parse(window.localStorage.getItem("auth"))
-            axios.get("https://www.googleapis.com/drive/v3/files", { headers: { Authorization: 'Bearer ' + auth.access_token } })
-                .then((res) => {
-                    this.files = res.data.files
-                })
-                .catch(err => {
-                    console.error(err)
-                })
-        },
-    },
-}
-</script>
+  </template>
+  
+  <script>
+  import { googleSdkLoaded } from 'vue3-google-login';
+  import axios from 'axios';
+  import CardComponent from '@/components/CardComponent.vue'
+  
+  export default {
+      name: 'GoogleLogin',
+      components: {
+          CardComponent
+      },
+      data() {
+          return {
+              auth: null
+          }
+      },
+      mounted() {
+          this.listFolders();  
+      },
+      methods: {
+          login() {
+              {
+                  googleSdkLoaded((google) => {
+                      google.accounts.oauth2.initCodeClient({
+                          client_id: '532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com',
+                          scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.metadata https://www.googleapis.com/auth/drive.photos.readonly',
+                          callback: this.initializeToken
+                      }).requestCode()
+                  })
+              }
+          },
+          initializeToken(response){
+              let payload = {
+                  code: response.code,
+                  client_id: "532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
+                  client_secret: "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",
+                  redirect_uri: "http://localhost:8080",
+                  grant_type: "authorization_code"
+              }
+  
+              axios.post("https://oauth2.googleapis.com/token", payload)
+              .then((res) => {
+                  this.auth = res.data
+                  this.auth.expires_in = new Date().getTime() + res.data.expires_in * 1_000;
+                  store.commit('setGoogleAuth', {
+                      access_token: this.auth.access_token,
+                      expires_in: this.auth.expires_in
+                  });
+                  window.localStorage.setItem("google", this.auth.refresh_token)
+              })
+  
+          },
+          requestToken(callback){
+              if(new Date().getTime() < this.auth.expires_in){
+                  callback();
+                  return;
+              }
+                  
+              let payload = {
+                  refresh_token: this.auth.refresh_token,
+                  client_id: "532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
+                  client_secret: "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",
+                  grant_type: "refresh_token"
+              }
+  
+  
+              this.saveToken(payload, callback);
+          },
+          refreshToken(payload, callback){
+              console.log(payload);
+              axios.post("https://oauth2.googleapis.com/token", payload)
+              .then((res) => {
+                  this.auth = res.data
+                  this.auth.expires_in = new Date().getTime() + res.data.expires_in * 1_000;
+                  window.localStorage.setItem("auth", JSON.stringify(this.auth))
+              })
+              .then(callback());
+          },
+          listFolders(){
+              axios.get("https://www.googleapis.com/drive/v3/files", 
+              { headers: { Authorization: `Bearer ${store.getters.googleToken}`} })
+              .then((res) => {
+                  this.files = res.data.files
+                  console.log(this.files)
+              })
+              .catch(err => {
+                  console.error(err)
+              })
+          },
+      },
+  }
+  </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
   state: {
+    googleAuth: null
   },
   getters: {
+    async googleToken(){
+      let google = window.localStorage.getItem('google');
+
+      if(!google)
+        return null;
+
+      if(this.googleAuth == null || new Date().getTime() < this.googleAuth.expires_in){
+        let payload = {
+          refresh_token: google,
+          client_id: "532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
+          client_secret: "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",
+          grant_type: "refresh_token"
+        }
+        
+        await axios.post("https://oauth2.googleapis.com/token", payload)
+        .then((res) => {
+          this.googleAuth.access_token = res.data.access_token;
+          this.googleAuth.expires_in = new Date().getTime() + res.data.expires_in * 1_000;
+        })
+      }
+
+      return this.googleAuth.access_token;
+    }
   },
   mutations: {
+    setGoogleAuth(googleAuth){
+      this.googleAuth = googleAuth;
+    }
   },
   actions: {
+
   },
   modules: {
   }
