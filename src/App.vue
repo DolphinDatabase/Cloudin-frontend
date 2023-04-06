15912d64ebe2e120b45b8a8a69e03d8c19cc203c
Pedro Motta <pedromotta321r@gmail.com>
2023-04-06 14:10:08 -0300

    add event source listener

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,14 @@
 
 <style src="./assets/tailwind.css">
 </style>
+
+<script>
+export default {
+  mounted(){
+    const eventSource = new EventSource('http://localhost:5000/listen');
+      eventSource.addEventListener('message', (event) => {
+        console.log(event)
+      });
+  }
+}
+</script>
