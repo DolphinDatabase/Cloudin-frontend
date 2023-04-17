9f67a02588c4e6251cb48ae19e3a5f8477716c76
Victor Cavichioli <vivictoraraujo@hotmail.com>
2023-04-17 20:56:23 -0300

    CLD-69 Formatação de Datas

--- src/components/cards/TransactionCard.vue
+++ src/components/cards/TransactionCard.vue
@@ -91,7 +91,7 @@
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap">
                     <div class="text-sm text-gray-900">
-                      {{ t.created }}
+                      {{ new Date(t.created).toLocaleDateString('pt-BR') }}
                     </div>
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap">
