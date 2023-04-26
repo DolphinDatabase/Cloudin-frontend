d28ab8166c26c325bd239f4daa5b900633020019
Abraao <abraao.vscncls@gmail.com>
2023-04-26 19:27:22 -0300

    feature/dashboard tela com estatísticas de transferências e power bi

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,18 @@
+<template>
+    <BasePage
+      title="Estatísticas de Transferências"    
+      :show-button="false"
+    >
+      <DashboardPage />
+    </BasePage>    
+  </template>
+  
+  <script>
+    import DashboardPage from '@/components/layout/DashboardPage.vue';
+    import BasePage from '@/components/layout/BasePage.vue';
+    
+    export default {
+        name: "DashboardView",
+        components: { BasePage, DashboardPage }
+    }
+  </script>
