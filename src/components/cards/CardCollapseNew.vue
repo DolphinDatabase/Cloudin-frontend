012bee2405507b157461ba11ae2ec5bb6ac90074
AbraaoHenriqueVasconcelos2 <84921620+AbraaoHenriqueVasconcelos2@users.noreply.github.com>
2023-05-26 21:49:48 -0300

    Update CardCollapseNew.vue
    toLowerCase

--- src/components/cards/CardCollapseNew.vue
+++ src/components/cards/CardCollapseNew.vue
@@ -87,12 +87,12 @@ export default {
   methods: {
     
     onOriginUpdated(updatedOrigin) {
-      this.origin = updatedOrigin; // Atualizar origin no componente pai
+      this.origin = updatedOrigin.toLowerCase();; // Atualizar origin no componente pai
       console.log("origin");
       console.log(this.origin);
     },
     onDestinyUpdated(updatedDestiny) {
-      this.destiny = updatedDestiny; // Atualizar destiny no componente pai
+      this.destiny = updatedDestiny.toLowerCase();; // Atualizar destiny no componente pai
       console.log("destiny");
       console.log(this.destiny);
     },
